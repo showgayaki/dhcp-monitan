6f7c5f822b623d54661fcbb7942b662a91cd94fe
showgayaki <knk.lee.kj@gmail.com>
2023-10-16 15:29:32 +0900

    お掃除

--- next-app/src/layout/AdminLayout/Header/Header.tsx
+++ next-app/src/layout/AdminLayout/Header/Header.tsx
@@ -2,10 +2,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import Link from 'next/link'
 import { Button, Container } from 'react-bootstrap'
-import Breadcrumb from '@layout/AdminLayout/Breadcrumb/Breadcrumb'
-import HeaderFeaturedNav from '@layout/AdminLayout/Header/HeaderFeaturedNav'
-import HeaderNotificationNav from '@layout/AdminLayout/Header/HeaderNotificationNav'
-import HeaderProfileNav from '@layout/AdminLayout/Header/HeaderProfileNav'
 
 type HeaderProps = {
     toggleSidebar: () => void;
@@ -40,19 +36,6 @@ export default function Header(props: HeaderProps) {
                         <use xlinkHref="/assets/brand/coreui.svg#full" />
                     </svg>
                 </Link>
-                <div className="header-nav d-none d-md-flex">
-                    <HeaderFeaturedNav />
-                </div>
-                <div className="header-nav ms-auto">
-                    <HeaderNotificationNav />
-                </div>
-                <div className="header-nav ms-2">
-                    <HeaderProfileNav />
-                </div>
-            </Container>
-            <div className="header-divider border-top my-2 mx-sm-n2" />
-            <Container fluid>
-                <Breadcrumb />
             </Container>
         </header>
     )
