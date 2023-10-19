a7b63646de46fb0429ddd709e097c79250915db5
showgayaki <knk.lee.kj@gmail.com>
2023-10-19 23:21:27 +0900

    leasesの取得日付表示を追加

--- next-app/src/pages/leases.tsx
+++ next-app/src/pages/leases.tsx
@@ -10,6 +10,7 @@ import { ClientTable } from '@components/ClientTable'
 const Leases: NextPage = () => {
     const networksUrl = `${process.env.NEXT_PUBLIC_API_BASE_URL}clients` || ''
 
+    const [date, setDate] = useState('')
     const [fallbackResource, setFallbackResource] = useState<ClientList[]>(
         [{
             network_address: '',
@@ -21,7 +22,9 @@ const Leases: NextPage = () => {
     const { data: { data: resource } } = useSWRAxios<ClientList[]>({
         url: networksUrl,
         transformResponse: transformResponseWrapper((d: ClientList[]) => {
-            console.log(d)
+            const now = new Date();
+            setDate(now.toLocaleString())
+            console.log(`${date}:`, d)
             return d
         }),
     }, {
@@ -41,6 +44,7 @@ const Leases: NextPage = () => {
                 {resource.map((network, index) => {
                     return (
                         <Tab key={index} eventKey={network.network_address} title={network.network_address} className='py-3 px-2'>
+                            <p className='fs-7'>As of: {date}</p>
                             <div className="row">
                                 <div className="col-md-12 mb-3">
                                     <Card>
