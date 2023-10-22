bff18718931dd553253be95f66544a549249f277
showgayaki <knk.lee.kj@gmail.com>
2023-10-22 23:10:10 +0900

    title共通化

--- next-app/src/pages/_app.tsx
+++ next-app/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import '@styles/globals.scss'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 // Next.js allows you to import CSS directly in .js files.
 // It handles optimization and all the necessary Webpack configuration to make this work.
 import { config } from '@fortawesome/fontawesome-svg-core'
@@ -18,11 +19,16 @@ function MyApp({ Component, pageProps }: AppProps) {
     // to ensure that the auto-generated ids are consistent between the server and client.
     // https://react-bootstrap.github.io/getting-started/server-side-rendering/
     return (
+        <>
+        <Head>
+            <title>DHCP Monitan</title>
+        </Head>
         <SSRProvider>
             <ProgressBar />
             {/* eslint-disable-next-line react/jsx-props-no-spreading */}
             <Component {...pageProps} />
         </SSRProvider>
+        </>
     )
 }
 
--- next-app/src/pages/login.tsx
+++ next-app/src/pages/login.tsx
@@ -7,7 +7,6 @@ import {
 } from 'react-bootstrap'
 import Link from 'next/link'
 import { SyntheticEvent, useState, useRef, useEffect } from 'react'
-import Head from 'next/head'
 import { useRouter } from 'next/router'
 import axios from 'axios'
 import { deleteCookie, getCookie } from 'cookies-next'
@@ -57,94 +56,89 @@ const Login: NextPage = () => {
     }, []);
 
     return (
-        <>
-            <Head>
-                <title>DHCP Monitan</title>
-            </Head>
-            <div className="bg-light min-vh-100 d-flex flex-row align-items-center dark:bg-transparent">
-                <Container>
-                    <Row className="justify-content-center align-items-center px-3">
-                        <Col lg={8}>
-                            <Row>
-                                <Col md={7} className="bg-white border p-5">
-                                    <div className="">
-                                        <h1>Login</h1>
-                                        <p className="text-black-50">Sign In to your account</p>
+        <div className="bg-light min-vh-100 d-flex flex-row align-items-center dark:bg-transparent">
+            <Container>
+                <Row className="justify-content-center align-items-center px-3">
+                    <Col lg={8}>
+                        <Row>
+                            <Col md={7} className="bg-white border p-5">
+                                <div className="">
+                                    <h1>Login</h1>
+                                    <p className="text-black-50">Sign In to your account</p>
 
-                                        <form onSubmit={login}>
-                                            <InputGroup className="mb-3">
-                                                <InputGroup.Text>
-                                                    <FontAwesomeIcon
-                                                        icon={faUser}
-                                                        fixedWidth
-                                                    />
-                                                </InputGroup.Text>
-                                                <Form.Control
-                                                    ref={inputElement}
-                                                    name="username"
-                                                    required
-                                                    disabled={submitting}
-                                                    placeholder="Username"
-                                                    aria-label="Username"
-                                                    onChange={(e) => setUsername(e.target.value)}
+                                    <form onSubmit={login}>
+                                        <InputGroup className="mb-3">
+                                            <InputGroup.Text>
+                                                <FontAwesomeIcon
+                                                    icon={faUser}
+                                                    fixedWidth
                                                 />
-                                            </InputGroup>
+                                            </InputGroup.Text>
+                                            <Form.Control
+                                                ref={inputElement}
+                                                name="username"
+                                                required
+                                                disabled={submitting}
+                                                placeholder="Username"
+                                                aria-label="Username"
+                                                onChange={(e) => setUsername(e.target.value)}
+                                            />
+                                        </InputGroup>
 
-                                            <InputGroup className="mb-3">
-                                                <InputGroup.Text>
-                                                    <FontAwesomeIcon
-                                                        icon={faLock}
-                                                        fixedWidth
-                                                    />
-                                                </InputGroup.Text>
-                                                <Form.Control
-                                                    type="password"
-                                                    name="password"
-                                                    required
-                                                    disabled={submitting}
-                                                    placeholder="Password"
-                                                    aria-label="Password"
-                                                    onChange={(e) => setPassword(e.target.value)}
+                                        <InputGroup className="mb-3">
+                                            <InputGroup.Text>
+                                                <FontAwesomeIcon
+                                                    icon={faLock}
+                                                    fixedWidth
                                                 />
-                                            </InputGroup>
+                                            </InputGroup.Text>
+                                            <Form.Control
+                                                type="password"
+                                                name="password"
+                                                required
+                                                disabled={submitting}
+                                                placeholder="Password"
+                                                aria-label="Password"
+                                                onChange={(e) => setPassword(e.target.value)}
+                                            />
+                                        </InputGroup>
 
-                                            <Row>
-                                                <Col xs={6}>
-                                                    <Button className="px-4" variant="primary" type="submit" disabled={submitting}>Login</Button>
-                                                </Col>
-                                                <Col xs={6} className="text-end">
-                                                    <Button className="px-0" variant="link" type="submit">
-                                                        Forgot
-                                                        password?
-                                                    </Button>
-                                                </Col>
-                                            </Row>
-                                        </form>
-                                    </div>
-                                </Col>
-                                <Col
-                                    md={5}
-                                    className="bg-primary text-white d-flex align-items-center justify-content-center p-5"
-                                >
-                                    <div className="text-center">
-                                        <h2>Sign up</h2>
-                                        <p>
-                                            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                                            tempor incididunt ut labore et dolore magna aliqua.
-                                        </p>
-                                        <Link href="/register">
-                                            <button className="btn btn-lg btn-outline-light mt-3" type="button">
-                                                Register Now!
-                                            </button>
-                                        </Link>
-                                    </div>
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-        </>
+                                        <Row>
+                                            <Col xs={6}>
+                                                <Button className="px-4" variant="primary" type="submit" disabled={submitting}>Login</Button>
+                                            </Col>
+                                            <Col xs={6} className="text-end">
+                                                <Button className="px-0" variant="link" type="submit">
+                                                    Forgot
+                                                    password?
+                                                </Button>
+                                            </Col>
+                                        </Row>
+                                    </form>
+                                </div>
+                            </Col>
+                            <Col
+                                md={5}
+                                className="bg-primary text-white d-flex align-items-center justify-content-center p-5"
+                            >
+                                <div className="text-center">
+                                    <h2>Sign up</h2>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                        tempor incididunt ut labore et dolore magna aliqua.
+                                    </p>
+                                    <Link href="/register">
+                                        <button className="btn btn-lg btn-outline-light mt-3" type="button">
+                                            Register Now!
+                                        </button>
+                                    </Link>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     )
 }
 
